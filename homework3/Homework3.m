ff710b1bb0a35b9217b9030ae26541ca50c97341
Alex Tacescu <alextac98@gmail.com>
2020-02-19 17:09:59 -0500

    Add output to trajectory generation to output a constants for variable sized inputes

--- homework3/Homework3.m
+++ homework3/Homework3.m
@@ -34,3 +34,29 @@ invMC= inv(M)*C;
 
 q0 = [pi; pi/2];
 qf = [0; 0];
+
+
+%% Part 2: Generate Desired Trajectory
+q0_2 = [0; 0];
+qf_2 = [pi/3; pi/4];
+t0_2 = 0; tf_2 = 10;
+v0_2 = [0; 0];
+vf_2 = [0; 0];
+
+TwoLinkArmTraj(q0_2, qf_2, v0_2, vf_2, t0_2, tf_2)
+
+%% TODO: GENERATE TRAJECTORY USING TwoLinkArmTraj matlab file.
+%% You need to specify the initial state, final state (in the state variables of the state space form), the time span.
+%% If quintic trajectories are to be generated, consider specify the desired accelerations for all joints.
+
+%% Implement the inverse dynamic control  
+options = odeset('RelTol',1e-4,'AbsTol',[1e-4, 1e-4, 1e-4, 1e-4]);
+[T,X] = ode45(@(t,x) inverseDC(t,x),[0 tf],x0, options);
+
+%% Plotting the result: please plot both the actual trajectory and the desired trajectory for the your states. example code below.
+% figure('Name','Theta_1 under inverse dynamic control');
+% plot(T, X(:,1),'r-');
+% hold on
+% figure('Name','Theta_2 under inverse dynamic control');
+% plot(T, X(:,2),'r--');
+% hold on
--- homework3/TwoLinkArmTraj.m
+++ homework3/TwoLinkArmTraj.m
@@ -2,14 +2,16 @@ function output = TwoLinkArmTraj(q0, qf, v0, vf, t0, tf)
 %TWOLINKARMTRAJ Cubic Polynomial function to determine constants for a
 %trajectory
 
-q_vector = [q0; v0; qf; vf];
+for i = 1:size(q0)
+q_vector = [q0(i); v0(i); qf(i); vf(i)];
 
 a_matrix = [1, t0, t0^2,   t0^3;
             0,  1, 2*t0, 3*t0^2;
             1, tf, tf^2,   tf^3;
             0,  1, 2*tf, 3*tf^2];
 
-output = linsolve(a_matrix, q_vector);
+output(i, :) = linsolve(a_matrix, q_vector);
 
+end
 end
 
